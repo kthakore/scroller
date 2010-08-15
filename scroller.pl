c492f04397f0cf1f7a04006fb40a7cf5ac20cf39
Kartik Thakore <thakore.kartik@gmail.com>
2010-08-15 01:05:06 -0400

    Have a runner now

--- scroller.pl
+++ scroller.pl
@@ -1,3 +1,39 @@
+package Runner;
+
+use base 'SDLx::Controller::Object';
+
+sub new {
+    my $class   = shift;
+    my %options = @_;
+    my $self    = SDLx::Controller::Object->new(%options);
+
+    my $accel = sub {
+
+        my ( $t, $state ) = @_;
+        my $k  = 10;
+        my $b  = 1;
+        my $ax = ( ( -1 * $k ) * ( $state->x ) - $b * $state->v_x );
+
+        return ( 10, 0, 0 );
+    };
+
+    $self->set_acceleration($accel);
+    $self->acceleration(1);
+    $options{app}->add_object( $self, \&render, $options{app} );
+    $self = bless $self, $class;
+    return $self;
+}
+
+sub render {
+    my $state = shift;
+    my $app   = shift;
+    $app->draw_rect( [ 0,         0,         $app->w, $app->h ], 0x0 );
+    $app->draw_rect( [ $state->x, $state->y, 2,       2 ],       0xFF0FFF );
+    $app->update();
+
+}
+
+package main;
 use strict;
 use warnings;
 use Time::HiRes qw( time sleep );
@@ -6,35 +42,16 @@ use SDLx::App;
 use SDL::Event;
 use SDL::Events;
 
-use lib 'lib';
-use Object;
+use SDLx::Controller::Object;
+my $app = SDLx::App->new( w => 200, h => 200, title => "timestep" );
 
-my $quit;
-my $spring       = Object->new();
-my $t            = 0.0;
-my $dt           = 0.01;
-my $current_time = 0.0;
-my $accumulator  = 0.0;
-my $app          = SDLx::App->new( w => 200, h => 200, title => "timestep" );
-my $event        = SDL::Event->new();
-while ( !$quit ) {
-    SDL::Events::pump_events();
-    while ( SDL::Events::poll_event($event) ) {
-        $quit = 1 if $event->type == SDL_QUIT;
-    }
-    my $new_time   = time;
-    my $delta_time = $new_time - $current_time;
-    $current_time = $new_time;
-    $delta_time = 0.25 if ( $delta_time > 0.25 );
-    $accumulator += $delta_time;
-    while ( $accumulator >= $dt ) {
-        $accumulator -= $dt;
-        $spring->update( $t, $dt );
-        $t += $dt;
-    }
-    my $state = $spring->interpolate( $accumulator / $dt );
-    $app->draw_rect( [ 0, 0, $app->w, $app->h ], 0x0 );
-    $app->draw_rect( [ 98, int( $state->{x} ), 2, 2 ], 0xFF0FFF );
-    $app->update();
-}
+my $spring = Runner->new( x => 0, y => 150, app => $app );
+
+my $event = sub {
+    return 0 if $_[0]->type == SDL_QUIT;
+    return 1;
+};
+
+$app->add_event_handler($event);
+$app->run_test();
 
